d6635b2040c7a67b10de7da47344d6330d58799f
XuShaoWei <swhd0501@163.com>
2016-09-18 14:31:10 +0800

    Initial commit

---
+++ src/com/youmeek/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.youmeek.demo;
+
+/**
+ * Created by dell on 2016/9/18.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
